4f82cbe02c0ed32c20f7e58d5cd6518246a0ee14
abel3ri <abelmerete@yahoo.com>
2024-05-27 19:45:07 +0300

    add cityList component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
 
 function App() {
   return (
@@ -15,8 +16,8 @@ function App() {
         <Route path="/product" element={<Product />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/app" element={<AppLayout />}>
-          <Route index element={<h1>Cities</h1>} />
-          <Route path="cities" element={<h1>Cities</h1>} />
+          <Route index element={<CityList />} />
+          <Route path="cities" element={<CityList />} />
           <Route path="countries" element={<h1>Countries</h1>} />
           <Route path="form" element={<h1>Form</h1>} />
         </Route>
---
+++ src/components/CityList.jsx
@@ -0,0 +1,6 @@
+import styles from "./CityList.module.css";
+function CityList() {
+  return <div className={styles.cityList}>CITIES LIST</div>;
+}
+
+export default CityList;
