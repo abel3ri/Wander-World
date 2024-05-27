90fc45b1e109c551b98559f7333f4e5384733253
abel3ri <abelmerete@yahoo.com>
2024-05-27 19:40:36 +0300

    implement nested route

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,12 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/product" element={<Product />} />
         <Route path="/pricing" element={<Pricing />} />
-        <Route path="/app" element={<AppLayout />} />
+        <Route path="/app" element={<AppLayout />}>
+          <Route index element={<h1>Cities</h1>} />
+          <Route path="cities" element={<h1>Cities</h1>} />
+          <Route path="countries" element={<h1>Countries</h1>} />
+          <Route path="form" element={<h1>Form</h1>} />
+        </Route>
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
