f6d2902c6d94007b64930eb4117a0a4d0aebf8a9
abel3ri <abelmerete@yahoo.com>
2024-05-31 23:04:39 +0300

    add key prop

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -21,7 +21,7 @@ function CountryList({ cities, isLoading }) {
   return (
     <ul className={styles.countryList}>
       {countries.map((country) => {
-        return <CountryItem country={country} />;
+        return <CountryItem country={country} key={country.country} />;
       })}
     </ul>
   );
